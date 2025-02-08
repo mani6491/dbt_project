8f5a4a32cd08b4efa3a229e25602b0fb4b6d2f5f
mani6491 <write2mani64@gmail.com>
2025-02-08 15:17:22 +0530

    Update department_IT.sql

--- models/example/department_IT.sql
+++ models/example/department_IT.sql
@@ -6,7 +6,7 @@ first_name,
 last_name,
 departments.department_id,
 department_name
-from employees inner join departments
+from "employees" inner join "departments"
 where employees.department_id=departments.department_id
 and department_name ='IT'
 
